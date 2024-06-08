818b9286679ea96bd05b799f6f3adcc1a5468d98
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-06-09 01:13:51 +0300

    update amount

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import { Web3 } from 'web3';
 
-const RPC = 'http://IP:8545';
+const RPC = 'http://88.99.32.171:8545';
 const httpProvider = new Web3.providers.HttpProvider(RPC);
 const web3 = new Web3(httpProvider);
 
 // PRIV_KEY 0x ile başlamalı
-const wallet = web3.eth.accounts.wallet.add('PRIV_KEY');
+const wallet = web3.eth.accounts.wallet.add(
+  '0x6e7dbc5c5eb5d2067613548019cee1192eccb5ea1a3cd7b3285edb8174e7d1cd'
+);
 let txCount = 0;
 
 async function send() {
   try {
     const to = web3.eth.accounts.create();
-    const val = Math.random().toString();
+    const val = (Math.random() * 0.1).toString();
     const amount = web3.utils.toWei(val, 'ether');
 
     await web3.eth.sendTransaction({
